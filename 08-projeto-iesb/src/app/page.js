b7c9572260cdb8b457fa01cfdc38fa9032ef6e25
Gustavo-dev-it <tavintwd@gmail.com>
2024-10-23 21:44:24 -0300

    AA

---
+++ 08-projeto-iesb/src/app/page.js
@@ -0,0 +1,18 @@
+import Pagina from '@/components/Pagina'
+import React from 'react'
+
+export default function Homepage() {
+  return (
+   <Pagina titulo={"Projeto IESB"}>
+
+
+
+
+
+
+
+
+
+   </Pagina>
+  )
+}
